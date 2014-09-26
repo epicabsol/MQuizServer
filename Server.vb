122ba5a6145529736a03d01947f7a2b6a1697c61
epicabsol <pokeboygarcia@gmail.com>
2014-09-26 16:28:22 -0700

    Added the server

---
+++ Server.vb
@@ -0,0 +1,71 @@
+﻿Imports System.Threading
+Imports System.Net
+
+Module Server
+    Private ServerThread As Thread
+    Private PleaseExit As Boolean = False
+    Private Exited As Boolean = False
+    Private Listener As New HttpListener
+    Private _serverrunning As Boolean = False
+    Public ReadOnly Property ServerRunning As Boolean
+        Get
+            Return _serverrunning
+        End Get
+    End Property
+    Public Sub StartServer()
+        ServerThread = New Thread(AddressOf RunServerThreadLoop)
+        ServerThread.Start()
+    End Sub
+    Public Sub StopServerAsync()
+        PleaseExit = True
+    End Sub
+    Public Sub StopServer()
+        PleaseExit = True
+        Listener.Stop()
+        Do While Exited = False
+            Thread.Sleep(0)
+        Loop
+    End Sub
+    Private Sub RunServerThreadLoop()
+        _serverrunning = True
+        PleaseExit = False
+        Exited = False
+
+        Listener.Prefixes.Add("http://localhost:5041/")
+        Listener.Start()
+        Dim c As HttpListenerContext
+        Do While PleaseExit = False
+            Try
+                c = Listener.GetContext()
+                Respond(c)
+            Catch ex As Exception
+                'Listener.Stop was (probably) called, so no worries here.
+            End Try
+        Loop
+        Listener.Stop()
+
+        Exited = True
+        _serverrunning = False
+    End Sub
+
+    Private Sub Respond(c As HttpListenerContext)
+        Dim inputbuffer() As Byte = {}
+        Dim inputstring As String
+        c.Request.InputStream.Read(inputbuffer, 0, c.Request.ContentLength64)
+        inputstring = System.Text.UTF8Encoding.UTF8.GetString(inputbuffer)
+        Dim responseString As String = ""
+        If Strings.Left(c.Request.RawUrl, 6) = "/auth-" Then
+            Dim code As String = Decrypt(Strings.Right(c.Request.RawUrl, Len(c.Request.RawUrl) - 6))
+            If code = GetTeacherCode() Then
+                responseString = "good"
+            Else
+                responseString = "bad"
+            End If
+        End If
+
+        Dim response() As Byte = System.Text.Encoding.UTF8.GetBytes(responseString)
+        c.Response.ContentLength64 = response.Length
+        c.Response.OutputStream.Write(response, 0, response.Length)
+        c.Response.OutputStream.Close()
+    End Sub
+End Module
